ebeb6b2a7221eaf694f8ebb59580cd9d29798c5c
Charlton Dias <charltondias29@gmail.com>
2023-11-24 01:56:54 +0530

    tasks functions refactoring

--- src/screens/Tasks.tsx
+++ src/screens/Tasks.tsx
@@ -1,69 +1,27 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {Heading, ScrollView, VStack, View} from '@gluestack-ui/themed';
 import Card from '../components/Card';
 import styles from '../styles';
-import {RefreshControl} from 'react-native';
 import NoTaskNotice from '../components/NoTaskNotice';
-import {fetchTasks} from '../functions/tasks';
-
-export type TaskDataProps = {
-  selectedBy: string | undefined;
-  id: string;
-  creator: string;
-  date: any;
-  description?: string;
-  images?: string[];
-  status?: string;
-  taskName: string;
-};
+import {useAppSelector} from '../redux/hooks';
+import {TaskDataProps} from '../types';
 
 const Tasks = () => {
-  const [activeTasks, setActiveTasks] = useState<TaskDataProps[]>([]);
-  const [expiredTasks, setExpiredTasks] = useState<TaskDataProps[]>([]);
-  const [completedTasks, setCompletedTasks] = useState<TaskDataProps[]>([]);
-  const [refreshing, setRefreshing] = useState(false);
+  const {data} = useAppSelector(state => state.tasks);
+  const availableTasks = data.filter(task => task.status === 'new');
+  const completedTasks = data.filter(task => task.status === 'completed');
+  const takenTasks = data.filter(task => task.status === 'taken');
 
-  const onRefresh = React.useCallback(async () => {
-    setRefreshing(true);
-    await fetchData();
-    setRefreshing(false);
-  }, []);
-
-  const fetchData = async () => {
-    const {active, expired, completed} = await fetchTasks();
-    setActiveTasks(active);
-    setExpiredTasks(expired);
-    setCompletedTasks(completed);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
   return (
     <>
-      <ScrollView
-        px={10}
-        contentContainerStyle={styles.taskContainer}
-        refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-        }>
-        <TaskSection
-          title="Available"
-          data={activeTasks}
-          fetchData={fetchData}
-        />
-        <TaskSection
-          title="Completed"
-          data={completedTasks}
-          fetchData={fetchData}
-        />
-        {expiredTasks.length > 0 && (
-          <TaskSection
-            title="Expired"
-            data={expiredTasks}
-            fetchData={fetchData}
-          />
+      <ScrollView px={10} contentContainerStyle={styles.taskContainer}>
+        <TaskSection title="Available" data={availableTasks} />
+        <TaskSection title="Completed" data={completedTasks} />
+
+        {takenTasks.length > 0 && (
+          <TaskSection title="Taken" data={takenTasks} />
         )}
+
         <View mb={40} />
       </ScrollView>
     </>
@@ -75,39 +33,29 @@ export default Tasks;
 type TaskSectionProps = {
   title: string;
   data: TaskDataProps[];
-  fetchData: () => void;
 };
 
-const TaskSection: React.FC<TaskSectionProps> = ({title, data, fetchData}) => (
-  <>
-    <Heading ml={10} size="xl">
-      {title}
-    </Heading>
-    {data?.length > 0 ? (
-      <ScrollView
-        horizontal
-        showsHorizontalScrollIndicator
-        indicatorStyle="black"
-        marginBottom={20}>
-        <VStack display="flex" flexDirection="row" marginBottom={2}>
-          {data.map((item, index) => (
-            <Card
-              key={index}
-              creator={item.creator}
-              date={item.date}
-              description={item?.description}
-              id={item.id}
-              images={item?.images}
-              status={item?.status}
-              taskName={item.taskName}
-              selectedBy={item.selectedBy}
-              fetchData={fetchData}
-            />
-          ))}
-        </VStack>
-      </ScrollView>
-    ) : (
-      <NoTaskNotice title={title} />
-    )}
-  </>
-);
+const TaskSection: React.FC<TaskSectionProps> = ({title, data}) => {
+  return (
+    <>
+      <Heading ml={10} size="xl">
+        {title}
+      </Heading>
+      {data?.length > 0 ? (
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator
+          indicatorStyle="black"
+          marginBottom={20}>
+          <VStack display="flex" flexDirection="row" marginBottom={2}>
+            {data.map((item, index) => (
+              <Card key={index} task={item} />
+            ))}
+          </VStack>
+        </ScrollView>
+      ) : (
+        <NoTaskNotice title={title} />
+      )}
+    </>
+  );
+};
