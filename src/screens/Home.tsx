37cb1e5b997331792bfbb05c0457f12c7a1bd6cf
Charlton Dias <charltondias29@gmail.com>
2023-11-23 16:19:15 +0530

    Add MapView component and update Home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from 'react';
-import Leaflet, {Layers, Markers, TileOptions} from 'react-native-leaflet-ts';
 import {Fab} from '@gluestack-ui/themed';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import {getCurrentLocation} from '../functions/location';
+import MapViews from '../components/MapView';
 
 const Home = () => {
   const [region, setRegion] = useState([15.4617259147707, 73.83342337687071]);
@@ -16,32 +16,6 @@ const Home = () => {
     updateLocation();
   }, []);
 
-  const options: TileOptions = {
-    noWrap: false,
-    detectRetina: true,
-    updateWhenIdle: true,
-  };
-
-  const mapLayers: Layers[] = [
-    {
-      name: 'Street View',
-      src: 'https://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}',
-      tileOptions: options,
-    },
-  ];
-
-  const markerList: Markers[] = [
-    {
-      latLng: region,
-      iconSize: {
-        width: 20,
-        height: 20,
-      },
-      title: 'Your location',
-      disabled: true,
-    },
-  ];
-
   return (
     <>
       <Fab
@@ -51,18 +25,7 @@ const Home = () => {
         bottom={80}>
         <MaterialIcons name="my-location" size={24} color="white" />
       </Fab>
-      <Leaflet
-        mapLayers={mapLayers}
-        minZoom={2}
-        zoom={13}
-        maxZoom={17}
-        flyTo={{
-          latLng: region,
-          zoom: 15,
-        }}
-        markers={markerList}
-        startInLoadingState
-      />
+      <MapViews region={region} />
     </>
   );
 };
